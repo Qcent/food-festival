ce63a8d877de98cbe282160f923e17f60839d201
Dave Quinn <dquinn8@cogeco.ca>
2021-12-21 14:12:48 -0500

    installed service-worker to manage caches of files for offline viewing

--- service-worker.js
+++ service-worker.js
@@ -13,7 +13,13 @@ const FILES_TO_CACHE = [
     "./dist/app.bundle.js",
     "./dist/events.bundle.js",
     "./dist/tickets.bundle.js",
-    "./dist/schedule.bundle.js"
+    "./dist/schedule.bundle.js",
+    "./assets/img/burgers.jpg",
+    "./assets/img/chips.jpg",
+    "./assets/img/falafel.jpg",
+    "./assets/img/food-stall.jpg",
+    "./assets/img/food-table.jpg",
+    "./assets/img/grill.jpg",
 ];
 
 self.addEventListener('install', function(e) {
@@ -23,4 +29,39 @@ self.addEventListener('install', function(e) {
             return cache.addAll(FILES_TO_CACHE)
         })
     )
-})
+});
+
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            let cacheKeeplist = keyList.filter(function(key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map(function(key, i) {
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function(request) {
+            if (request) { // if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else { // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+});
